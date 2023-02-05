741b1492cb04a3baa8ccc8ffaf4c6cd3f74ad6e1
Ragul p <66929382+ragulpalanisamy@users.noreply.github.com>
2023-02-05 16:04:05 +0530

    initial Commit

--- Dashboard/main.js
+++ Dashboard/main.js
@@ -1,31 +1,24 @@
-function show(){
-  var password = document.getElementById("password");
-  var icon = document.querySelector(".fas")
 
-  // ========== Checking type of password ===========
-  if(password.type === "password"){
-    password.type = "text";
+document.getElementById("signup").onclick = function(){
+  window.location.assign("\login.html");
+}
+
+document.getElementById("sign").onclick = function(){
+  window.location.assign("\index.html");
+}
+
+document.getElementById("loginbutton").onclick = function(){
+
+  let username = document.getElementById("uname").value;
+  let password = document.getElementById("password").value;
+
+  if(username == "admin@gmail.com" && password == "admin123"){
+    window.location.assign("dashboard.html");
+    alert("You Successfully LogedIn!.....");
   }
-  else {
-    password.type = "password";
+  else{
+    window.location.assign("index.html");
+    alert("Invalid Details");
+
   }
-};
-
-// Get the login button and form elements
-const loginBtn = document.getElementById("login-btn");
-const loginForm = document.getElementById("login-form");
-
-// Add an event listener to the login button
-loginBtn.addEventListener("click", function() {
-  // Get the login details from the form
-  const email = loginForm.elements.email.value;
-  const password = loginForm.elements.password.value;
-
-  // Validate the login details
-  if (email === "test@example.com" && password === "password") {
-    // Redirect to the dashboard page
-    window.location.href = "dashboard.html";
-  } else {
-    alert("Invalid login details. Please try again.");
-  }
-});
+}
